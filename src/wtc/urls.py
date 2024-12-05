1e28c0767d2b677dae544fb2143f8a7e07365e26
hptouchscreencmo <hptouchscreencmo@gmail.com>
2024-12-05 10:49:27 +0530

    into the main

---
+++ src/wtc/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import *
+from .views import search_wtc_records
+
+# from . import views
+
+urlpatterns = [
+    path('wtcsubmitdata/', TextDataView.as_view(), name='text-data'),
+    path('wtc/records/', WTCListViewAnaly.as_view(), name='wtc-list'),
+    path('wtc/sentiment-summary/', SentimentSummaryView.as_view(), name='wtc-sentiment-summary'),
+    path('wtc/lo-scru-summary/', ScrutinySummaryView.as_view(), name='wtc-sentiment-summary'),
+    path('wtc/requests-per-day/', RequestsPerDayView.as_view(), name='requests-per-day'),
+    path('wtc/mode-distribution/', ModeDistributionView.as_view(), name='mode-distribution'),
+    path('wtc/depr-rout-distribution/', DeprRoutDistributionView.as_view(), name='depr-rout-distribution'),
+    path('wtc/analytics/', AnalyticsAPIView.as_view(), name='analytics'),
+    path('wtc/type-distribution/', TypeDistributionView.as_view(), name='type-distribution'),
+    path('wtc/scrutiny-statistics/', WTCStatisticsAPIView.as_view(), name='wtc_statistics_api'),
+    path('search_wtc_records/', search_wtc_records, name='search_wtc_records'),
+    path('primary-scrutiny-done/', primary_scrutiny_done, name='primary_scrutiny_done'),
+    path('wtc/lo/', WTCLOListView.as_view(), name='wtc_lo'),
+
+
+]
